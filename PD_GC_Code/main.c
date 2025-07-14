a4c8cde85fc77364f5502a9551937ed1c9555f67
Genilson Silva De Araújo Junior <61212256+GenilsonJrs@users.noreply.github.com>
2025-07-14 16:30:53 -0300

    Iniciando Implementação

--- PD_GC_Code/main.c
+++ PD_GC_Code/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+#define TAM 10000
 
 int main() {
     int m, moedas[100], alvo;
+    int dp[TAM];
     system("cls || clear");
 
     printf("Quantidade de moedas: ");
@@ -15,6 +19,18 @@ int main() {
 
     printf("Valor que deseja pagar: ");
     scanf("%d", &alvo);
+
+    for (int i = 0; i <= alvo; i++) dp[i] = INT_MAX;
+    dp[0] = 0;
+
+    for (int v = 1; v <= alvo; v++) {
+        for (int i = 0; i < m; i++) {
+            int c = moedas[i];
+            if (v - c >= 0 && dp[v - c] != INT_MAX) {
+
+            }
+        }
+    }
 
     printf("\n\n");
 
